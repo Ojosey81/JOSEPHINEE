199f5f933ad20bebcbaab7600ebf98efbbfe8f06
JOSEPHINEE <105178612+Ojosey81@users.noreply.github.com>
2022-06-24 13:19:02 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function zuri() {
+    alert("Assignment");
+}
+var name=prompt("what is your name?"); 
+console.log("Hello "+name)
+
+var height=prompt("what is your height");
+console.log("i am "+height)
+
+var country=prompt("what country are you from");
+console.log("I am from  "+country)
+ 
